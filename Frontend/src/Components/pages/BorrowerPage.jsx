ac6d3ca2cebfc6a820fe7116a040c1b01e653a91
dawipayandas <dawipayandas@gmail.com>
2025-07-04 05:52:50 +0530

    fix: fixed loader background

--- Frontend/src/Components/pages/BorrowerPage.jsx
+++ Frontend/src/Components/pages/BorrowerPage.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import BookService from '../../services/BookService';
 import { Link, useNavigate } from 'react-router-dom';
 import './BorrowerPage.css'
